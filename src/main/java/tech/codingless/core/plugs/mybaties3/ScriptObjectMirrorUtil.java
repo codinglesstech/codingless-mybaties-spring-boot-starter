2d4471da03ff6c49e2c8bfb391deed85693f5d07
wanghongyan <258156979@qq.com>
2022-08-12 19:52:16 +0800

    clear

--- src/main/java/tech/codingless/core/plugs/mybaties3/ScriptObjectMirrorUtil.java
+++ src/main/java/tech/codingless/core/plugs/mybaties3/ScriptObjectMirrorUtil.java
@@ -1,14 +1,9 @@
 package tech.codingless.core.plugs.mybaties3;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-
-import jdk.nashorn.api.scripting.ScriptObjectMirror;
-
 public class ScriptObjectMirrorUtil {
-
-	@SuppressWarnings("removal")
+ 
 	public static Object to(Object obj, Class<?> clazz) {
+		/*
 		ScriptObjectMirror scriptObj = (ScriptObjectMirror) obj;
 		try { 
 			JSONObject json = new JSONObject();
@@ -20,6 +15,7 @@ public class ScriptObjectMirrorUtil {
 		} catch (Exception e) { 
 			e.printStackTrace();
 		} 
+		*/
 		return null; 
 	}
 
