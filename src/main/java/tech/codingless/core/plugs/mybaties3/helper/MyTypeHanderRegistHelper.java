7aa101f0e2e59d29ed8616937a2de3312dffb182
wanghongyan <258156979@qq.com>
2023-06-15 20:15:18 +0800

    shutdown when found onetype user to head

--- src/main/java/tech/codingless/core/plugs/mybaties3/helper/MyTypeHanderRegistHelper.java
+++ src/main/java/tech/codingless/core/plugs/mybaties3/helper/MyTypeHanderRegistHelper.java
@@ -7,17 +7,20 @@ import org.apache.ibatis.type.TypeHandlerRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * 
  * 
  * @author 王鸿雁
  * @version 2021年10月5日
  */
+@Slf4j
 public class MyTypeHanderRegistHelper {
 	private static final Logger LOG = LoggerFactory.getLogger(MyTypeHanderRegistHelper.class);
 	private static ConcurrentHashMap<Class<?>, Boolean> CACHE = new ConcurrentHashMap<>();
+	private static ConcurrentHashMap<Class<?>, Class<?>> MAP = new ConcurrentHashMap<>();
 
- 
 	public static void regist(Configuration configuration, Class<?> clazz) {
 		if (CACHE.containsKey(clazz)) {
 			return;
@@ -32,8 +35,18 @@ public class MyTypeHanderRegistHelper {
 					if (registry.hasTypeHandler(columnProp.getTypeHandler())) {
 						return;
 					}
-					LOG.info("Regist Type Hander:{} For ColumnType:{}, JavaType:{}", columnProp.getTypeHandler(),columnProp.getJdbcType(),columnProp.getJavaTypeClass());
+					if(LOG.isDebugEnabled()) {
+						LOG.debug("Regist Type Hander:{} For ColumnType:{}, JavaType:{}", columnProp.getTypeHandler(), columnProp.getJdbcType(), columnProp.getJavaTypeClass());
+					}
+					if (MAP.containsKey(columnProp.getJavaTypeClass()) && MAP.get(columnProp.getJavaTypeClass()) != columnProp.getTypeHandler()) {
+
+						Error error = new Error(String.format("The Type:%s duplicate Regist Type Handler (%s)  and (%s)", columnProp.getJavaTypeClass(), columnProp.getTypeHandler(),
+								MAP.get(columnProp.getJavaTypeClass())));
+						log.error("Important Error Cause System exist ", error);
+						System.exit(0);
+					}
 					configuration.getTypeHandlerRegistry().register(columnProp.getJavaTypeClass(), columnProp.getJdbcType(), columnProp.getTypeHandler());
+					MAP.put(columnProp.getJavaTypeClass(), columnProp.getTypeHandler());
 				}
 			} catch (Throwable e) {
 				CACHE.remove(clazz);
